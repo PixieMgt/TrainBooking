97cfba56ff35626b24392e4ec32db365d79b7f2a
VanHaverbekeBram <92434333+VanHaverbekeBram@users.noreply.github.com>
2023-05-15 18:58:32 +0200

    Booking en ticket DAO - Service toegevoegd

---
+++ TrainBooking.Services/BookingService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainBooking.Models.Entities;
+using TrainBooking.Repositories.Interfaces;
+using TrainBooking.Services.Interfaces;
+
+namespace TrainBooking.Services
+{
+    public class BookingService : IService<Booking>
+    {
+        private readonly IDAO<Booking> _bookingDao;
+        public BookingService(IDAO<Booking> bookingDao)
+        {
+            _bookingDao = bookingDao;
+        }
+
+        public async Task Add(Booking entity)
+        {
+            await _bookingDao.Add(entity);
+        }
+
+        public Task Delete(Booking entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Booking> FindById(int id)
+        {
+            return await _bookingDao.FindById(id);
+        }
+
+        public async Task<IEnumerable<Booking>> GetAll()
+        {
+            return await _bookingDao.GetAll();
+        }
+
+        public Task Update(Booking entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrainBooking.Services/TicketService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainBooking.Models.Entities;
+using TrainBooking.Repositories.Interfaces;
+using TrainBooking.Services.Interfaces;
+
+namespace TrainBooking.Services
+{
+    public class TicketService : IService<Ticket>
+    {
+        private IDAO<Ticket> _ticketDao;
+        public TicketService(IDAO<Ticket> ticketDao)
+        {
+            _ticketDao = ticketDao;
+        }
+        public async Task Add(Ticket entity)
+        {
+            await _ticketDao.Add(entity);
+        }
+
+        public Task Delete(Ticket entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Ticket> FindById(int id)
+        {
+            return await _ticketDao.FindById(id);
+        }
+
+        public async Task<IEnumerable<Ticket>> GetAll()
+        {
+            return await _ticketDao.GetAll();
+        }
+
+        public Task Update(Ticket entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
