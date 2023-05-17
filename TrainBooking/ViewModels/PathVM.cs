70a7862ea48369fb62ce2ea8e91cf7a9e26eb2c3
VanHaverbekeBram <92434333+VanHaverbekeBram@users.noreply.github.com>
2023-05-17 17:37:27 +0200

    api werkt correct

---
+++ TrainBooking/ViewModels/PathVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrainBooking.ViewModels
+{
+    public class PathVM
+    {
+        public PathVM()
+        {
+            SectionsVM = new();
+        }
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public List<SectionVM?> SectionsVM { get; set; }
+
+    }
+}
